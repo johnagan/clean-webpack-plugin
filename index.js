e1d010a97b123c880ac716a43584774b652f1806
John Agan <johnagan@github.com>
2015-04-05 17:10:01 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var rimraf = require('rimraf')
+var path = require('path');
+var join = path.join;
+
+function Plugin(paths) {
+  // determine webpack root
+  this.context = path.dirname(module.parent.filename);
+
+  // allows for a single string entry
+  if (typeof paths == 'string' || paths instanceof String){
+    paths = [paths];
+  }
+
+  // store paths
+  this.paths = paths;
+}
+
+Plugin.prototype.apply = function(compiler) {
+  var self = this;
+
+  // preform an rm -rf on each path
+  self.paths.forEach(function(path){
+    var path = join(self.context, path);
+    rimraf.sync(path);
+  });
+}
+
+module.exports = Plugin;
